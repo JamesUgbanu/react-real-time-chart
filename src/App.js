2b7ea4f490144521f52ce1715a6e949836364e89
James Ugbanu <jamesugbanu@gmail.com>
2020-10-20 12:20:11 +0100

    react realtime chart

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Box,
+  Card,
+  CardHeader,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import clsx from 'clsx';
+import Chart from './Chart';
+import logo from './logo.svg'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+  },
+  current: {
+    marginTop: theme.spacing(0.5),
+    marginRight: theme.spacing(0.5)
+  },
+}));
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const RealTimeChart = ({ className, ...rest }) => {
+  const classes = useStyles();
+  const [data, setData] = useState([
+    136,
+    176,
+    116,
+    195,
+    98,
+    136,
+    145,
+    166,
+    167,
+    183
+  ]);
+
+  const getData = () => {
+
+    setTimeout(() => {
+        setData((prevData) => {
+          const newData = [...prevData];
+          const random = getRandomInt(100, 200);
+
+          newData.pop();
+          newData.push(random);
+
+          return newData;
+        });
+    }, 500);
+  };
+
+  useEffect(() => {
+    setInterval(() => getData(), 2000);
+  }, []);
+
+  const labels = data.map((value, i) => i);
+
+  return (
+    <Container
+        maxWidth="sm"
+      >
+        <Box
+          mb={4}
+          display="flex"
+          justifyContent="center"
+        >
+           <img
+          alt="Logo"
+          src={logo}
+          />
+        </Box>
+        <Box
+          mb={8}
+          display="flex"
+          justifyContent="center"
+        >
+          <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardHeader
+        action={(
+          <Typography
+            color="inherit"
+            variant="h3"
+          >
+            {
+              data[data.length - 1]
+            }
+          </Typography>
+        )}
+        classes={{ action: classes.current }}
+        subheader="Page views per second"
+        title="Active users"
+      />
+      <Chart
+        data={data}
+        labels={labels}
+      />
+    </Card>
+ 
+     </Box>
+    </Container>       
+  );
+}
+
+export default RealTimeChart;
